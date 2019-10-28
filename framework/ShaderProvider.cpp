3f4de544907b190095259a34db8afaabf99c1a3a
Alan Prado <alanpradoaraujo@gmail.com>
2019-10-27 23:24:08 -0300

    Rm texture from shader

--- framework/ShaderProvider.cpp
+++ framework/ShaderProvider.cpp
@@ -1,12 +1,10 @@
 #include "ShaderProvider.hpp"
 
 const char* ShaderProvider::getVertexShader(){
-    return "\nattribute vec4 a_position;\n attribute vec2 a_textureCoord;\n"
+    return "\nattribute vec4 a_position;\n"
                 "uniform mat4 u_mvpMatrix;\n"
-                "varying vec2 v_textureCoord;\n"
                 "void main() {\n"
                    "gl_Position = u_mvpMatrix * a_position;\n"
-                   "v_textureCoord= a_textureCoord;\n"
                 "}\n";
 }
 
