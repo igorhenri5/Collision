6aae24aa40d6b90ce170a0aebbfb0f41b217bf35
Igor Henrique <igorhenri5@hotmail.com>
2019-11-15 18:38:26 -0300

    q

--- quadtree/Quadtree.hpp
+++ quadtree/Quadtree.hpp
@@ -1,8 +1,23 @@
 #ifndef __QTREE__
 #define __QTREE__
 
+#include <vector>
+#define MAX_ENTITIES 4
 class QuadTree{
+private:
+	QuadTree* nodes;
+	std::vector<?> entityList;
+	int level;
 
+public:
+	QuadTree(int level);
+	~QuadTree();
+
+	void clear();
+	void split();
+	void addEntity(?);
+	int getPlaceIndex(?);
+	std::vector<?> retrieve(? entity, List returnObjects);
 };
 
 #endif
