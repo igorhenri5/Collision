f5280c0b23204b72510e4a01cf6967ade2623975
Igor Henrique <igorhenri5@hotmail.com>
2019-11-15 21:54:58 -0300

    Qtree fix

--- quadtree/Quadtree.cpp
+++ quadtree/Quadtree.cpp
@@ -1,32 +1,34 @@
 #include "Quadtree.hpp"
 #include "Rect.hpp"
 
-QuadTree::QuadTree(int level, Rect bounds){
+QuadTree::QuadTree(int level, Rect* bounds){
 	this->nodes  = new QuadTree[4];
 	this->level  = level;
 	this->bounds = bounds;
 }
 
 QuadTree::~QuadTree(){
+	for(int i=0; i<4; i++)
+		delete nodes[i];
 	delete[] nodes;
-	delete  bounds;
+	delete   bounds;
 }
 
-QuadTree::void clear(){
+void QuadTree::clear(){
 	entityList.clear();
 	for(int i=0; i<4; i++){
 		if(nodes[i] != NULL){
-			nodes[i].clear();
-			nodes[i] = NULL;
+			nodes[i]->clear();
+			delete nodes[i];
 		}
 	}
 }
 
-QuadTree::void split(){
-	int nodeWidth  = (int)(bounds.getWidth() /2);
-	int nodeHeight = (int)(bounds.getHeight()/2);
-	int x = (int)bounds.getX();
-	int y = (int)bounds.getY();
+void QuadTree::split(){
+	int nodeWidth  = (int)(bounds->getWidth() /2);
+	int nodeHeight = (int)(bounds->getHeight()/2);
+	int x = (int)bounds->getX();
+	int y = (int)bounds->getY();
 
 	nodes[0] = new Quadtree(level+1, new Rectangle(x + nodeWidth, y, nodeWidth, nodeHeight));
 	nodes[1] = new Quadtree(level+1, new Rectangle(x, y, nodeWidth, nodeHeight));
@@ -34,12 +36,12 @@ QuadTree::void split(){
 	nodes[3] = new Quadtree(level+1, new Rectangle(x + nodeWidth, y + nodeHeight, nodeWidth, nodeHeight));
 }
 
-QuadTree::void add(Rect entity){
+void QuadTree::add(Rect* entity){
 
 	if(nodes[0] != NULL){
 		int index = getPlaceIndex(entity);
 		if(index != -1){
-			nodes[index].addEntity(entity);
+			nodes[index]->addEntity(entity);
 			return;
 		}
 	}
@@ -51,9 +53,9 @@ QuadTree::void add(Rect entity){
 			split();
 
 		for(std::vector<int>::iterator it = entityList.begin(); it != entityList.end(); it++){
-			int index = getPlaceIndex(entityList.(it);
+			int index = getPlaceIndex(entityList.at(it));
 			if(index != -1){
-				nodes[index].push_back(it);
+				nodes[index]->add(it);
 				entityList.erase(it);
 			}else{
 				i++;
@@ -63,18 +65,18 @@ QuadTree::void add(Rect entity){
 }
 
 //Descobrir em qual subQuadrante o objeto de tipo ? se encaixaria na arvore
-QuadTree::int getPlaceIndex(Rect entity){
+int QuadTree::getPlaceIndex(Rect* entity){
 	int index = -1;
 
-	double centralX = bounds.getX() + (bounds.getWidth() /2);
-	double centralY = bounds.getY() + (bounds.getHeight()/2);
+	double centralX = bounds->getX() + (bounds->getWidth() /2);
+	double centralY = bounds->getY() + (bounds->getHeight()/2);
 
-	boolean topQuad    = (entity.getY() < centralY && entity.getY() + entity.getHeight() < centralY);
-	boolean bottomQuad = (entity.getY() > centralY);
+	boolean topQuad    = (entity->getY() < centralY && entity->getY() + entity->getHeight() < centralY);
+	boolean bottomQuad = (entity->getY() > centralY);
 
 //  0 1
 //  3 2
-	if(entity.getX() < centralX && entity.getX() + entity.getWidth() < centralX) {
+	if(entity->getX() < centralX && entity->getX() + entity->getWidth() < centralX) {
 		if(topQuad){
 			index = 0;
 		}
@@ -82,7 +84,7 @@ QuadTree::int getPlaceIndex(Rect entity){
 			index = 3;
 		}
 	}
-	else if(entity.getX() > centralX){
+	else if(entity->getX() > centralX){
 		if(topQuad){
 			index = 1;
 		}
@@ -94,10 +96,10 @@ QuadTree::int getPlaceIndex(Rect entity){
 }
 
 //Retornar um vector com os objetos que possivelmente podem colidir com o objeto do parametro
-QuadTree::std::vector<?>* retrieve(Rect entity){
+std::vector<Rect*>* QuadTree::retrieve(Rect* entity){
 	int index = getPlaceIndex(entity);
-	if(index != -1 && nodes[0] != null){
-		return nodes[index].retrieve(entity);
+	if(index != -1 && nodes[0] != NULL){
+		return nodes[index]->retrieve(entity);
 	}
-	return &returnObjects;
+	return &entityList;
  }
--- quadtree/Quadtree.hpp
+++ quadtree/Quadtree.hpp
@@ -5,9 +5,9 @@
 #define MAX_ENTITIES 4
 class QuadTree{
 private:
-	QuadTree* nodes;
-	Rect bounds;
-	std::vector<Rect> entityList;
+	QuadTree** nodes;
+	Rect* bounds;
+	std::vector<Rect*> entityList;
 	int level;
 
 public:
