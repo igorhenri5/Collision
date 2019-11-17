97ec628f5c3429a3f1688ee38104051286db2572
Alan Prado <alanpradoaraujo@gmail.com>
2019-11-17 17:58:33 -0300

    Change background color blue->white

--- main.cpp
+++ main.cpp
@@ -128,7 +128,7 @@ void initOpenGLEnvironment(int width, int height){
     glEnable(GL_DEPTH_TEST);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_BLEND);
-    glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // Set background frame color
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // Set background frame color
     glViewport(0, 0, width, height);
 }
 
