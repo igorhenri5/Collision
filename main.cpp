8193a5d228aaf75ac80e33aa9e6de70778360f8f
Igor Henrique <igorhenri5@hotmail.com>
2019-11-20 14:19:48 -0300

    fix x

--- main.cpp
+++ main.cpp
@@ -123,7 +123,7 @@ void parallelHandleAllCollisions(){
 
     if(x<=0){
         std::cout << "Mnt ";
-        printElapsedTime(elapsedTimeMount / 100);
+        printElapsedTime(elapsedTimeMount / 10);
         elapsedTimeMount = 0;
     }
     game::masterFlag->reset(pairLists->size());
@@ -155,7 +155,7 @@ void update(){
     elapsedTimeAdd += getSeconds(&tempoInicial, &tempoFinal);
     if(x<=0){
         std::cout << "Add ";
-        printElapsedTime(elapsedTimeAdd / 100);
+        printElapsedTime(elapsedTimeAdd / 10);
         elapsedTimeAdd = 0;
     }
 
@@ -166,7 +166,7 @@ void update(){
     elapsedTimeCld += getSeconds(&tempoInicial, &tempoFinal);
     if(x<=0){
         std::cout << "Cld ";
-        printElapsedTime(elapsedTimeCld / 100);
+        printElapsedTime(elapsedTimeCld / 10);
         elapsedTimeCld = 0;
     }
 
@@ -180,7 +180,7 @@ void update(){
     elapsedTimeUpt += getSeconds(&tempoInicial, &tempoFinal);
     if(x<=0){
         std::cout << "Upt ";
-        printElapsedTime(elapsedTimeUpt / 100);
+        printElapsedTime(elapsedTimeUpt / 10);
         elapsedTimeUpt = 0;
     }
 
@@ -191,7 +191,7 @@ void update(){
     elapsedTimeCln += getSeconds(&tempoInicial, &tempoFinal);
     if(x<=0){
         std::cout << "Cln ";
-        printElapsedTime(elapsedTimeCln / 100);
+        printElapsedTime(elapsedTimeCln / 10);
         elapsedTimeCln = 0;
     }
 }
@@ -204,10 +204,10 @@ void mainloop(){
     elapsedTimeAll += getSeconds(&tempoInicialAll, &tempoFinalAll);
     if(x<=0){
         std::cout << "All ";
-        printElapsedTime(elapsedTimeAll / 100);
+        printElapsedTime(elapsedTimeAll / 10);
         std::cout << std::endl;
         elapsedTimeAll = 0;
-        x=100;
+        x=10;
     }
     x--;
 }
